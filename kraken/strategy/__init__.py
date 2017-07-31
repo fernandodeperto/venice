817be437352841ae5ddad61dde7a3e1d15897d3b
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-07-31 02:48:43 -0300

    Prepare the constants that will be used

--- kraken/strategy/__init__.py
+++ kraken/strategy/__init__.py
@@ -1,3 +1,13 @@
+"""
+Basic strategy package module.
+"""
 class KrakenStrategy:
+    """
+    Basic strategy class, supposed to be inherited.
+    """
+    LONG = 1
+    NEUTRAL = 0
+    SHORT = -1
+
     def __init__(self):
         pass
