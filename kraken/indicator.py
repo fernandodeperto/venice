d9c6066d6d7d2a1915df602670c73fde97880875
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-08-02 11:08:11 -0300

    Refactor the RSI code

--- kraken/indicator.py
+++ kraken/indicator.py
@@ -31,6 +31,7 @@ def mom(source, length):
 
     return series
 
+
 def rsi(source, length):
     """
     Relative strengh index.
@@ -47,14 +48,7 @@ def rsi(source, length):
         avg_gain.append((avg_gain[i - 1] * (length - 1) + gain[i]) / length)
         avg_loss.append((avg_loss[i - 1] * (length - 1) + loss[i]) / length)
 
-    rs = [avg_gain[x]/avg_loss[x] if avg_loss[x] > sys.float_info.epsilon else 0 for x in range(0, len(avg_gain))]
-    rsi = [100 - (100/(1 + rs[x])) for x in range(0, len(rs))]
+    rel_strength = [avg_gain[x]/avg_loss[x] if avg_loss[x] > sys.float_info.epsilon else 0 for x in
+                    range(0, len(avg_gain))]
 
-    return rsi
-
-
-if  __name__ == "__main__":
-    source = [44.34, 44.09, 44.15, 43.61, 44.33, 44.83, 45.10, 45.42, 45.84, 46.08, 45.89, 46.03,
-              45.61, 46.28, 46.28, 46.00, 46.03, 46.41, 46.22, 45.64, 46.21, 46.25, 45.71, 46.45,
-              45.78, 45.35, 44.03, 44.18, 44.22, 44.57, 43.42, 42.66, 43.13]
-    result = rsi(source, 14)
+    return [100 - (100/(1 + rel_strength[x])) for x in range(0, len(rel_strength))]
