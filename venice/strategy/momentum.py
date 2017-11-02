fcd2d9ccc57d0f417d01db4bbd0fbe6e84e9f695
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-11-02 21:06:04 -0200

    Update the momentum strategy

--- venice/strategy/momentum.py
+++ venice/strategy/momentum.py
@@ -2,7 +2,7 @@ import logging
 
 from .indicator import mom
 from .strategy import Strategy
-
+from venice.util import EPSILON
 
 
 class MomentumStrategy(Strategy):
@@ -10,6 +10,10 @@ class MomentumStrategy(Strategy):
         super().__init__(api, *args, **kwargs)
 
         self.length = length
+
+        self.current = None
+        self.buy = None
+        self.sell = None
 
     @staticmethod
     def descr_text():
@@ -26,11 +30,39 @@ class MomentumStrategy(Strategy):
     def run(self):
         logger = logging.getLogger(__name__)
 
-        mom0 = mom(self.api.close(limit=self.length), self.length)
+        ticker = self.api.ticker()
+        ohlc = self.api.ohlc(limit=self.length)
+        close = [x.close for x in ohlc]
+
+        mom0 = mom(close, self.length)
         mom1 = mom(mom0, 1)
 
-        logger.debug(mom0)
-        logger.debug(mom1)
+        logger.info('mom0={:.5f}, mom1={:.5f}'.format(mom0[-1], mom1[-1]))
+
+        if mom0[-1] > EPSILON and mom1[-1] > EPSILON:
+            self.buy = ohlc.high[-1]
+            logger.info('stop buy @ {:.5f}'.format(self.buy))
+
+        elif self.current and mom0[-1] < -EPSILON and mom1[-1] < -EPSILON:
+            self.sell = ohlc.low[-1]
+            logger.info('stop sell @ {:.5f}'.format(self.sell))
+
+        elif self.buy or self.sell:
+            self.buy = None
+            self.sell = None
+            logger.info('cancel')
+
+        if self.buy and ticker.last - self.buy > EPSILON:
+            self.buy = None
+            self.current = ticker.last
+            logger.info('close buy order @ {:.5f}'.format(self.current))
+
+        elif self.sell and ticker.last - self.sell < -EPSILON:
+            self.sell = None
+            logger.info('close buy order @ {:.5f} with sell order @ {:.5f}'.format(
+                self.current, ticker.last))
+
+            self.current = None
 
     def clean_up(self):
         pass
