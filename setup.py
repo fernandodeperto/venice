d6f5767232421e4c23a634e288427d3ee6fd1bdb
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-11-07 00:43:22 -0200

    Change version to 0.9rc1

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Kraken exchange CLI.
+Venice exchange trading application.
 """
 
 # Always prefer setuptools over distutils
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9',
+    version='0.9rc1',
 
     description='Cryptocurrency exchange tool',
     long_description=long_description,
