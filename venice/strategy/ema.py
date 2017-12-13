cbeab6d3cfa7d68b2d40db5757b95698d6ae0247
Fernando Mendonca <fernando.mendonca@autotrac.com.br>
2017-12-13 14:57:18 -0200

    Update the EMA strategy with 2 new options

--- venice/strategy/ema.py
+++ venice/strategy/ema.py
@@ -1,11 +1,12 @@
 from logging import getLogger
+from time import time
 
 from .strategy import Strategy
 from .indicator import ema
 
 
 class EMAStrategy(Strategy):
-    def __init__(self, api, fast_ema, slow_ema, cross, *args, **kwargs):
+    def __init__(self, api, fast_ema, slow_ema, cross, limit, *args, **kwargs):
         logger = getLogger(__name__)
 
         super().__init__(api, *args, **kwargs)
@@ -13,11 +14,13 @@ class EMAStrategy(Strategy):
         self.fast_ema = fast_ema
         self.slow_ema = slow_ema
         self.previous_ema = None
+        self.previous_order = None
 
         self.current = None
         self.pending = None
 
         self.cross = cross
+        self.limit = limit
 
         logger.debug(
             'ema strategy started with fast_ema={}, slow_ema={}, cross={}'.format(
@@ -33,7 +36,8 @@ class EMAStrategy(Strategy):
 
     @staticmethod
     def configure_parser(parser):
-        parser.add_argument('-c', '--cross', action="store_true", help="enable EMA cross")
+        parser.add_argument('-c', '--cross', action='store_true', help='enable EMA cross')
+        parser.add_argument('-l', '--limit', action='store_true', help='enable order limit')
         parser.add_argument('fast_ema', type=int, help='Fast EMA period')
         parser.add_argument('slow_ema', type=int, help='Slow EMA period')
 
@@ -61,13 +65,26 @@ class EMAStrategy(Strategy):
                     self.current = None
                     self.pending = None
 
-        if ema_fast >= ema_slow and (not self.cross or (self.previous_ema and
-                                                        self.previous_ema < ema_slow)):
-            if not self.current and not self.pending:
+        if ema_fast >= ema_slow and not self.current and not self.pending:
+            if self.cross and (not self.previous_ema or self.previous_ema >= ema_slow):
+                logger.debug('EMA is higher but cross has not been achieved')
+
+            elif (self.limit and self.previous_order and
+                  time() < self.previous_order + self.period * 30):
+                logger.debug('EMA is higher but order limit has been reached')
+
+            else:
                 self.pending = self.api.order_buy('EMA', self.api.MARKET)
 
-        else:
-            if self.current and not self.pending:
+        elif ema_fast < ema_slow and self.current and not self.pending:
+            if self.cross and self.previous_ema and self.previous_ema >= ema_slow:
+                logger.debug('EMA is lower but cross has not been achieved')
+
+            elif (self.limit and self.previous_order and
+                  time() < self.previous_order + self.period * 30):
+                logger.debug('EMA is lower but order limit has been reached')
+
+            else:
                 self.pending = self.api.order_sell('EMA', self.api.MARKET)
 
         if self.cross:
