ffde095d4843812e56589f8a3a87b727708c4403
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-12-21 01:05:05 -0200

    Update the EMA strategy

--- venice/strategy/ema.py
+++ venice/strategy/ema.py
@@ -8,7 +8,7 @@ from ..util import EPSILON
 
 
 class EMAStrategy(Strategy):
-    def __init__(self, api, fast_ema, slow_ema, cross, limit, *args, **kwargs):
+    def __init__(self, api, fast_ema, slow_ema, cross, epsilon, *args, **kwargs):
         logger = getLogger(__name__)
 
         super().__init__(api, *args, **kwargs)
@@ -19,14 +19,14 @@ class EMAStrategy(Strategy):
         self.cross = cross
         self.first_cross = False
 
-        self.limit = Decimal.from_float(limit)
+        self.epsilon = Decimal.from_float(epsilon) if epsilon else EPSILON
 
         self.current = None
         self.pending = None
 
-        logger.debug(
-            'ema strategy started with fast_ema={:.5f}, slow_ema={:.5f}, cross={:.5f}, '
-            'limit={}'.format(self.fast_ema, self.slow_ema, self.cross, self.limit))
+        logger.debug('ema strategy started with fast_ema={:.5f}, slow_ema={:.5f}, cross={}, '
+                     'epsilon={:.5f}'.format(
+                         self.fast_ema, self.slow_ema, self.cross, self.epsilon))
 
     @staticmethod
     def descr_text():
@@ -39,19 +39,20 @@ class EMAStrategy(Strategy):
     @staticmethod
     def configure_parser(parser):
         parser.add_argument('-c', '--cross', action='store_true', help='enable EMA cross')
-        parser.add_argument('-l', '--limit', type=float, help='limit EMA difference')
+        parser.add_argument('-e', '--epsilon', type=float, help='use a specific epsilon')
         parser.add_argument('fast_ema', type=int, help='Fast EMA period')
         parser.add_argument('slow_ema', type=int, help='Slow EMA period')
 
     @property
     def log_file(self):
-        return 'ema-{}-{}-{}-{}-{}'.format(
-            self.api.pair, self.fast_ema, self.slow_ema, self.cross, self.limit)
+        return 'ema-{}-{}-{}-{}'.format(self.api.pair, self.fast_ema, self.slow_ema, self.cross)
 
     def run(self):
         logger = getLogger(__name__)
 
-        close = [x.close for x in self.api.ohlc(limit=100)]
+        close = self.api.close(limit=100)
+        high = self.api.high(limit=100)
+        low = self.api.low(limit=100)
 
         ema_fast = ema(close, self.fast_ema)
         ema_slow = ema(close, self.slow_ema)
@@ -75,19 +76,15 @@ class EMAStrategy(Strategy):
                     self.current = None
                     self.pending = None
 
-        if (ema_fast[-1] - ema_slow[-1] > (self.limit if self.limit else EPSILON) and
-                not self.current and not self.pending):
+        if ema_fast[-1] - ema_slow[-1] > self.epsilon and not self.current and not self.pending:
             if self.cross and not self.first_cross:
                 logger.debug('EMA is higher but first cross has not been achieved')
 
             else:
-                self.pending = self.api.order_buy('EMA', self.api.MARKET)
-                self.previous_order = time()
+                self.pending = self.api.order_buy('EMA', self.api.STOP, price=high[-1])
 
-        elif (ema_fast[-1] - ema_slow[-1] < (-self.limit if self.limit else -EPSILON) and
-              self.current and not self.pending):
-            self.pending = self.api.order_sell('EMA', self.api.MARKET)
-            self.previous_order = time()
+        elif ema_fast[-1] - ema_slow[-1] < -self.epsilon and self.current and not self.pending:
+            self.pending = self.api.order_sell('EMA', self.api.STOP, price=low[-1])
 
         logger.debug('current={}, pending={}'.format(self.current, self.pending))
 
